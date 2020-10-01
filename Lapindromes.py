61505cbc75dfdc7c0669b5276e88ff8d07da22bc
raksha27yadav <52619596+raksha27yadav@users.noreply.github.com>
2020-10-01 16:53:55 +0530

    Create Lapindromes.py

---
+++ Lapindromes.py
@@ -0,0 +1,30 @@
+def call(a,b):
+    h={}
+    for i in a:
+        if i in h:
+            h[i]+=1
+        else:
+            h[i]=1
+    for i in b:
+        if i in h:
+            h[i]-=1
+        else:
+            h[i]=1
+    for i in h:
+        if h[i]!=0:
+            return False
+    return True
+for i in range(int(input())):
+    n=input()
+    k=len(n)//2
+    if len(n)%2==0:
+        if call(n[:k],n[k:]):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if call(n[:k],n[k+1:]):
+            print("YES")
+        else:
+            print("NO")
+        
